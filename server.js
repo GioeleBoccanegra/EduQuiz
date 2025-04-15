32a377de88ea98badd5b6c759e06007307b715ad
giobocca03 <gio.bocca03@gmail.com>
2025-04-15 11:30:23 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,14 @@
+const quizDomande = [
+  {
+    id: 1,
+    domanda: 'Qual è la capitale della Francia?',
+    opzioni: ['Parigi', 'Londra', 'Madrid', 'Roma'],
+    rispostaCorretta: 'Parigi'
+  },
+  {
+    id: 2,
+    domanda: 'Quale linguaggio si usa per creare pagine web?',
+    opzioni: ['HTML', 'Python', 'Java', 'C++'],
+    rispostaCorretta: 'HTML'
+  }
+];
